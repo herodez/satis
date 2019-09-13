30f485029d7b3b17d54544e26daff28e9e1f8998
Mario Gonzalez <mgonzalez@optimeconsulting.com>
2019-09-13 09:32:04 -0500

    feat: Clear all html generation command and file

--- src/Builder/PackagesBuilder.php
+++ src/Builder/PackagesBuilder.php
@@ -20,8 +20,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class PackagesBuilder extends Builder
 {
-    /** @var string packages.json file name. */
-    private $filename;
     /** @var string included json filename template */
     private $includeFileName;
     /** @var array */
@@ -30,8 +28,7 @@ class PackagesBuilder extends Builder
     public function __construct(OutputInterface $output, string $outputDir, array $config, bool $skipErrors)
     {
         parent::__construct($output, $outputDir, $config, $skipErrors);
-
-        $this->filename = $this->outputDir . '/packages.json';
+        
         $this->includeFileName = $config['include-filename'] ?? 'include/all$%hash%.json';
     }
 
@@ -74,12 +71,9 @@ class PackagesBuilder extends Builder
                 $repo['providers'][$packageName] = current($includes);
             }
         } else {
-            $repo['includes'] = $this->dumpPackageIncludeJson($packagesByName, $this->includeFileName);
+            $repo = $this->dumpPackageIncludeJson($packagesByName, $this->includeFileName);
         }
-
-        $this->dumpPackagesJson($repo);
-
-        $this->pruneIncludeDirectories();
+        
     }
 
     private function findReplacements(array $packages, string $replaced): array
@@ -97,80 +91,22 @@ class PackagesBuilder extends Builder
         return $replacements;
     }
 
-    private function pruneIncludeDirectories(): void
-    {
-        $this->output->writeln('<info>Pruning include directories</info>');
-        $paths = [];
-        while ($this->writtenIncludeJsons) {
-            list($hash, $includesUrl) = array_shift($this->writtenIncludeJsons);
-            $path = $this->outputDir . '/' . ltrim($includesUrl, '/');
-            $dirname = dirname($path);
-            $basename = basename($path);
-            if (false !== strpos($dirname, '%hash%')) {
-                throw new \RuntimeException('Refusing to prune when %hash% is in dirname');
-            }
-            $pattern = '#^' . str_replace('%hash%', '([0-9a-zA-Z]{' . strlen($hash) . '})', preg_quote($basename, '#')) . '$#';
-            $paths[$dirname][] = [$pattern, $hash];
-        }
-        $pruneFiles = [];
-        foreach ($paths as $dirname => $entries) {
-            foreach (new \DirectoryIterator($dirname) as $file) {
-                foreach ($entries as $entry) {
-                    list($pattern, $hash) = $entry;
-                    if (preg_match($pattern, $file->getFilename(), $matches) && $matches[1] !== $hash) {
-                        $group = sprintf(
-                            '%s/%s',
-                            basename($dirname),
-                            preg_replace('/\$.*$/', '', $file->getFilename())
-                        );
-                        if (!array_key_exists($group, $pruneFiles)) {
-                            $pruneFiles[$group] = [];
-                        }
-                        // Mark file for pruning.
-                        $pruneFiles[$group][] = new \SplFileInfo($file->getPathname());
-                    }
-                }
-            }
-        }
-        // Get the pruning limit.
-        $offset = $this->config['providers-history-size'] ?? 0;
-        // Unlink to-be-pruned files.
-        foreach ($pruneFiles as $group => $files) {
-            // Sort to-be-pruned files base on ctime, latest first.
-            usort(
-                $files,
-                function (\SplFileInfo $fileA, \SplFileInfo $fileB) {
-                    return $fileB->getCTime() <=> $fileA->getCTime();
-                }
-            );
-            // If configured, skip files from the to-be-pruned files by offset.
-            $files = array_splice($files, $offset);
-            foreach ($files as $file) {
-                unlink($file->getPathname());
-                $this->output->writeln(
-                    sprintf(
-                        '<comment>Deleted %s</comment>',
-                        $file->getPathname()
-                    )
-                );
-            }
-        }
-    }
-
     private function dumpPackageIncludeJson(array $packages, string $includesUrl, string $hashAlgorithm = 'sha1'): array
     {
         $filename = str_replace('%hash%', 'prep', $includesUrl);
-        $path = $tmpPath = $this->outputDir . '/' . ltrim($filename, '/');
+        $path = $this->outputDir . '/' . ltrim($filename, '/');
 
         $repoJson = new JsonFile($path);
         $options = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
         if ($this->config['pretty-print'] ?? true) {
             $options |= JSON_PRETTY_PRINT;
         }
-
+        
+        
         $contents = $repoJson->encode(['packages' => $packages], $options) . "\n";
         $hash = hash($hashAlgorithm, $contents);
-
+    
+     
         if (false !== strpos($includesUrl, '%hash%')) {
             $this->writtenIncludeJsons[] = [$hash, $includesUrl];
             $filename = str_replace('%hash%', $hash, $includesUrl);
@@ -180,64 +116,9 @@ class PackagesBuilder extends Builder
                 $path = null;
             }
         }
-
-        if ($path) {
-            $this->writeToFile($path, $contents);
-            $this->output->writeln("<info>wrote packages to $path</info>");
-        }
-
+        
         return [
-            $filename => [$hashAlgorithm => $hash],
+            'algo' => $hashAlgorithm, 'hash' => $hash, 'content' => $contents, 'filename' => $filename
         ];
-    }
-
-    /**
-     * @throws \UnexpectedValueException
-     * @throws \Exception
-     */
-    private function writeToFile(string $path, string $contents): void
-    {
-        $dir = dirname($path);
-        if (!is_dir($dir)) {
-            if (file_exists($dir)) {
-                throw new \UnexpectedValueException(
-                    $dir . ' exists and is not a directory.'
-                );
-            }
-            if (!@mkdir($dir, 0777, true)) {
-                throw new \UnexpectedValueException(
-                    $dir . ' does not exist and could not be created.'
-                );
-            }
-        }
-
-        $retries = 3;
-        while ($retries--) {
-            try {
-                file_put_contents($path, $contents);
-                break;
-            } catch (\Exception $e) {
-                if ($retries) {
-                    usleep(500000);
-                    continue;
-                }
-
-                throw $e;
-            }
-        }
-    }
-
-    /**
-     * @param array $repo Repository information
-     */
-    private function dumpPackagesJson(array $repo): void
-    {
-        if (isset($this->config['notify-batch'])) {
-            $repo['notify-batch'] = $this->config['notify-batch'];
-        }
-
-        $this->output->writeln('<info>Writing packages.json</info>');
-        $repoJson = new JsonFile($this->filename);
-        $repoJson->write($repo);
     }
 }
--- src/Console/Command/BuildCommand.php
+++ src/Console/Command/BuildCommand.php
@@ -20,7 +20,6 @@ use Composer\Json\JsonFile;
 use Composer\Json\JsonValidationException;
 use Composer\Satis\Builder\ArchiveBuilder;
 use Composer\Satis\Builder\PackagesBuilder;
-use Composer\Satis\Builder\WebBuilder;
 use Composer\Satis\Console\Application;
 use Composer\Satis\PackageSelection\PackageSelection;
 use Composer\Util\RemoteFilesystem;
@@ -42,10 +41,12 @@ class BuildCommand extends BaseCommand
             ->setDefinition([
                 new InputArgument('file', InputArgument::OPTIONAL, 'Json file to use', './satis.json'),
                 new InputArgument('output-dir', InputArgument::OPTIONAL, 'Location where to output built files', null),
-                new InputArgument('packages', InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'Packages that should be built. If not provided, all packages are built.', null),
-                new InputOption('repository-url', null, InputOption::VALUE_OPTIONAL, 'Only update the repository at given url', null),
-                new InputOption('repository-strict', null, InputOption::VALUE_NONE, 'Also apply the repository filter when resolving dependencies'),
-                new InputOption('no-html-output', null, InputOption::VALUE_NONE, 'Turn off HTML view'),
+                new InputArgument('packages', InputArgument::IS_ARRAY | InputArgument::OPTIONAL,
+                    'Packages that should be built. If not provided, all packages are built.', null),
+                new InputOption('repository-url', null, InputOption::VALUE_OPTIONAL,
+                    'Only update the repository at given url', null),
+                new InputOption('repository-strict', null, InputOption::VALUE_NONE,
+                    'Also apply the repository filter when resolving dependencies'),
                 new InputOption('skip-errors', null, InputOption::VALUE_NONE, 'Skip Download or Archive errors'),
                 new InputOption('stats', null, InputOption::VALUE_NONE, 'Display the download progress bar'),
             ])
@@ -100,7 +101,7 @@ The json config file accepts the following keys:
 EOT
             );
     }
-
+    
     /**
      * @throws JsonValidationException
      * @throws ParsingException
@@ -112,12 +113,12 @@ EOT
         $configFile = $input->getArgument('file');
         $packagesFilter = $input->getArgument('packages');
         $repositoryUrl = $input->getOption('repository-url');
-        $skipErrors = (bool) $input->getOption('skip-errors');
-
+        $skipErrors = (bool)$input->getOption('skip-errors');
+        
         // load auth.json authentication information and pass it to the io interface
         $io = $this->getIO();
         $io->loadConfiguration($this->getConfiguration());
-
+        
         if (preg_match('{^https?://}i', $configFile)) {
             $rfs = new RemoteFilesystem($io);
             $contents = $rfs->getContents(parse_url($configFile, PHP_URL_HOST), $configFile, false);
@@ -126,12 +127,12 @@ EOT
             $file = new JsonFile($configFile);
             if (!$file->exists()) {
                 $output->writeln('<error>File not found: ' . $configFile . '</error>');
-
+                
                 return 1;
             }
             $config = $file->read();
         }
-
+        
         try {
             $this->check($configFile);
         } catch (JsonValidationException $e) {
@@ -153,44 +154,44 @@ EOT
             }
             $output->writeln(sprintf('<warning>%s: %s</warning>', get_class($e), $e->getMessage()));
         }
-
+        
         if (null !== $repositoryUrl && count($packagesFilter) > 0) {
             throw new \InvalidArgumentException('The arguments "package" and "repository-url" can not be used together.');
         }
-
+        
         // disable packagist by default
         unset(Config::$defaultRepositories['packagist'], Config::$defaultRepositories['packagist.org']);
-
+        
         if (!$outputDir = $input->getArgument('output-dir')) {
             $outputDir = $config['output-dir'] ?? null;
         }
-
+        
         if (null === $outputDir) {
             throw new \InvalidArgumentException('The output dir must be specified as second argument or be configured inside ' . $input->getArgument('file'));
         }
-
+        
         /** @var $application Application */
         $application = $this->getApplication();
         $composer = $application->getComposer(true, $config);
         $packageSelection = new PackageSelection($output, $outputDir, $config, $skipErrors);
-
+        
         if (null !== $repositoryUrl) {
-            $packageSelection->setRepositoryFilter($repositoryUrl, (bool) $input->getOption('repository-strict'));
+            $packageSelection->setRepositoryFilter($repositoryUrl, (bool)$input->getOption('repository-strict'));
         } else {
             $packageSelection->setPackagesFilter($packagesFilter);
         }
-
+        
         $packages = $packageSelection->select($composer, $verbose);
-
+        
         if (isset($config['archive']['directory'])) {
             $downloads = new ArchiveBuilder($output, $outputDir, $config, $skipErrors);
             $downloads->setComposer($composer);
             $downloads->setInput($input);
             $downloads->dump($packages);
         }
-
+        
         $packages = $packageSelection->clean();
-
+        
         if ($packageSelection->hasFilterForPackages() || $packageSelection->hasRepositoryFilter()) {
             // in case of an active filter we need to load the dumped packages.json and merge the
             // updated packages in
@@ -198,40 +199,30 @@ EOT
             $packages += $oldPackages;
             ksort($packages);
         }
-
+        
         $packagesBuilder = new PackagesBuilder($output, $outputDir, $config, $skipErrors);
         $packagesBuilder->dump($packages);
-
-        if ($htmlView = !$input->getOption('no-html-output')) {
-            $htmlView = !isset($config['output-html']) || $config['output-html'];
-        }
-
-        if ($htmlView) {
-            $web = new WebBuilder($output, $outputDir, $config, $skipErrors);
-            $web->setRootPackage($composer->getPackage());
-            $web->dump($packages);
-        }
-
+        
         return 0;
     }
-
+    
     private function getConfiguration(): Config
     {
         $config = new Config();
-
+        
         // add dir to the config
         $config->merge(['config' => ['home' => $this->getComposerHome()]]);
-
+        
         // load global auth file
         $file = new JsonFile($config->get('home') . '/auth.json');
         if ($file->exists()) {
             $config->merge(['config' => $file->read()]);
         }
         $config->setAuthConfigSource(new JsonConfigSource($file, true));
-
+        
         return $config;
     }
-
+    
     private function getComposerHome(): string
     {
         $home = getenv('COMPOSER_HOME');
@@ -248,10 +239,10 @@ EOT
                 $home = rtrim(getenv('HOME'), '/') . '/.composer';
             }
         }
-
+        
         return $home;
     }
-
+    
     /**
      * @throws ParsingException        if the json file has an invalid syntax
      * @throws JsonValidationException if the json file doesn't match the schema
@@ -259,33 +250,35 @@ EOT
     private function check(string $configFile): bool
     {
         $content = file_get_contents($configFile);
-
+        
         $parser = new JsonParser();
         $result = $parser->lint($content);
         if (null === $result) {
             if (defined('JSON_ERROR_UTF8') && JSON_ERROR_UTF8 === json_last_error()) {
                 throw new \UnexpectedValueException('"' . $configFile . '" is not UTF-8, could not parse as JSON');
             }
-
+            
             $data = json_decode($content);
-
+            
             $schemaFile = __DIR__ . '/../../../res/satis-schema.json';
             $schema = json_decode(file_get_contents($schemaFile));
             $validator = new Validator();
             $validator->check($data, $schema);
-
+            
             if (!$validator->isValid()) {
                 $errors = [];
-                foreach ((array) $validator->getErrors() as $error) {
+                foreach ((array)$validator->getErrors() as $error) {
                     $errors[] = ($error['property'] ? $error['property'] . ' : ' : '') . $error['message'];
                 }
-
-                throw new JsonValidationException('The json config file does not match the expected JSON schema', $errors);
+                
+                throw new JsonValidationException('The json config file does not match the expected JSON schema',
+                    $errors);
             }
-
+            
             return true;
         }
-
-        throw new ParsingException('"' . $configFile . '" does not contain valid JSON' . "\n" . $result->getMessage(), $result->getDetails());
+        
+        throw new ParsingException('"' . $configFile . '" does not contain valid JSON' . "\n" . $result->getMessage(),
+            $result->getDetails());
     }
 }
